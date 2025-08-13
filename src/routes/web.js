a164415c7a6c0c4d4b0055c7bd95083b787cbf77
trandat9029 <tranledatvp@gmail.com>
2025-08-13 15:06:32 +0700

    main: test write api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import homeController from '../controllers/homeController';
+import userController from '../controllers/userController';
 
 let router = express.Router();
 
@@ -17,6 +18,9 @@ let initWebRoutes = (app) =>{
     router.post('/put-crud', homeController.putCRUD);
     
     router.get('/delete-crud', homeController.deleteCRUD);
+
+
+    router.post('/api/login', userController.handleLogin);
 
     return app.use("/", router);
 }
