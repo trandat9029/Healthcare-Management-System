d0a6f631687f4e0bab77b7ad54b32b84598a1dd8
trandat9029 <tranledatvp@gmail.com>
2025-09-19 15:18:40 +0700

    main: add api return data detail doctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -12,12 +12,55 @@ let getTopDoctorHome = async (req, res) =>{
         console.log(error);
         return res.status(200).json({
             errCode: -1,
-            errorMessage: "Error from server!"
+            errMessage: "Error from server!"
+        })
+    }
+}
+
+let getAllDoctors = async (req, res) =>{
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server',
+        })
+    }
+} 
+ 
+let postInfoDoctor = async (req, res) =>{
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from the server!",
+        })
+    }
+}
+
+let getDetailDoctorById = async (req, res) =>{
+    try {
+        let info = await doctorService.getDetailDoctorByIdService(req.query.id);
+        return res.status(200).json(info);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server!'
         })
     }
 }
 
 module.exports = {
-    getTopDoctorHome,
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInfoDoctor: postInfoDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 
 }
